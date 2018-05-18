62bc5c873b7b033381c96a58d885349460ac1a14
DanielCM0000 <39316789+DanielCM0000@users.noreply.github.com>
2018-05-18 13:02:26 -0300

    Add files via upload

---
+++ Blink_ESP32.ino
@@ -0,0 +1,10 @@
+void setup(){ 
+  pinMode(LED_BUILTIN, OUTPUT);//LED_BUILTIN 2 == GPIO2
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));   
+  delay(250);
+}
+//NECESSÁRIO PRECIONAR O BOTÃO BOOT QUANDO COMEÇAR A CONEXÃO 
+//Connceting.........._____....
